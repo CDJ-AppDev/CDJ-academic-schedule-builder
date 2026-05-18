8aef71922f1d50e0dfe05e07c20ab41b97de2630
JianJimenez <jian.jimenez.main@gmail.com>
2026-05-18 14:19:27 +0800

    working admin

--- backend/sql/2users.sql
+++ backend/sql/2users.sql
@@ -2,7 +2,9 @@ WITH new_users AS (
   INSERT INTO user_credentials (useremail, userpassword, useraccess) VALUES
   ('jjt0965@dlsud.edu.ph', '$2a$10$ZUCOpXq3B.iONMPQkEL.5OXJol5nqMM.y/eAfESLBxXS1uicZTug2', 'Default'),  -- password: password123
   ('adrian@example.com', '$2a$10$ZUCOpXq3B.iONMPQkEL.5OXJol5nqMM.y/eAfESLBxXS1uicZTug2', 'Default'),  -- password: password123
-  ('csd0403@dlsud.edu.ph', '$2a$10$ZUCOpXq3B.iONMPQkEL.5OXJol5nqMM.y/eAfESLBxXS1uicZTug2', 'Default')  -- password: password123
+  ('csd0403@dlsud.edu.ph', '$2a$10$ZUCOpXq3B.iONMPQkEL.5OXJol5nqMM.y/eAfESLBxXS1uicZTug2', 'Default'),  -- password: password123
+  ('admin@gmail.com', '$2a$10$jKT5eHKnLfGSkriLO3oCo.7anit0t7es99rPRtwIIzaLaN9T7WH9e', 'Admin')  -- password: adminpass123
+  ON CONFLICT (useremail) DO NOTHING
   RETURNING userid, useremail
 )
 INSERT INTO user_profile (userid, username, termid) 
@@ -12,6 +14,11 @@ SELECT
     WHEN new_users.useremail = 'jjt0965@dlsud.edu.ph' THEN 'Jian Jimenez'
     WHEN new_users.useremail = 'adrian@example.com' THEN 'Adrian De Vega'
     WHEN new_users.useremail = 'csd0403@dlsud.edu.ph' THEN 'Samuel Carmona'
+    WHEN new_users.useremail = 'admin@gmail.com' THEN 'Super Admin'
   END,
-  'CS4'
-FROM new_users; 
+  CASE 
+    WHEN new_users.useremail = 'admin@gmail.com' THEN NULL
+    ELSE 'CS4'
+  END
+FROM new_users
+ON CONFLICT (userid) DO NOTHING;
