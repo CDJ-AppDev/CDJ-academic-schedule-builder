aeb930fb5f87279d7bf174549ca3a0b443954718
JianJimenez <jian.jimenez.main@gmail.com>
2026-05-20 20:01:30 +0800

    js refinement encryption

--- backend/sql/2users.sql
+++ backend/sql/2users.sql
@@ -1,9 +1,9 @@
 WITH new_users AS (
   INSERT INTO user_credentials (useremail, userpassword, useraccess) VALUES
-  ('jjt0965@dlsud.edu.ph', '$2a$10$ZUCOpXq3B.iONMPQkEL.5OXJol5nqMM.y/eAfESLBxXS1uicZTug2', 'Default'),  -- password: password123
-  ('adrian@example.com', '$2a$10$ZUCOpXq3B.iONMPQkEL.5OXJol5nqMM.y/eAfESLBxXS1uicZTug2', 'Default'),  -- password: password123
-  ('csd0403@dlsud.edu.ph', '$2a$10$ZUCOpXq3B.iONMPQkEL.5OXJol5nqMM.y/eAfESLBxXS1uicZTug2', 'Default'),  -- password: password123
-  ('admin@gmail.com', '$2a$10$jKT5eHKnLfGSkriLO3oCo.7anit0t7es99rPRtwIIzaLaN9T7WH9e', 'Admin')  -- password: adminpass123
+  ('jjt0965@dlsud.edu.ph', 'e3671fe79e46ed6148f414c50b44c8cd:fff164a6c87410cae4ed08f4e2b0cad4', 'Default'),  -- password: password123 (AES-256-CBC encrypted)
+  ('adrian@example.com', 'e3671fe79e46ed6148f414c50b44c8cd:fff164a6c87410cae4ed08f4e2b0cad4', 'Default'),  -- password: password123 (AES-256-CBC encrypted)
+  ('csd0403@dlsud.edu.ph', 'e3671fe79e46ed6148f414c50b44c8cd:fff164a6c87410cae4ed08f4e2b0cad4', 'Default'),  -- password: password123 (AES-256-CBC encrypted)
+  ('admin@gmail.com', 'b67a37b679c0f7e47114f170a7ab6a10:b17346125745a37fa3eaf4c040b29f32', 'Admin')  -- password: adminpass123 (AES-256-CBC encrypted)
   ON CONFLICT (useremail) DO NOTHING
   RETURNING userid, useremail
 )
